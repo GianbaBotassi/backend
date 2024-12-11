d3f92b0df9c1705d7764232ef151d1b7a38ee731
GianbaBotassi <gianba89@gmail.com>
2024-12-11 14:37:35 +0100

    Recupero lista boulder con filtri

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-require('dotenv').config();
-const fs = require('fs');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const fs = require("fs");
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -10,45 +10,51 @@ app.use(cors());
 app.use(express.json());
 
 // Connetti alla base di dati MongoDB
-mongoose.connect(process.env.MONGODB_URI, {
-});
+mongoose.connect(process.env.MONGODB_URI, {});
 
 // Definisci i modelli di dati
 const BoulderSchema = new mongoose.Schema({
   name: String,
   grade: String,
-  holds: String
+  holds: String,
 });
 
 const HoldsLayoutSchema = new mongoose.Schema({
   name: String,
-  holds: String
+  holds: String,
 });
 
-
-const Boulder = mongoose.model('Boulder', BoulderSchema);
-const HoldsLayout = mongoose.model('HoldsLayout', HoldsLayoutSchema);
+const Boulder = mongoose.model("Boulder", BoulderSchema);
+const HoldsLayout = mongoose.model("HoldsLayout", HoldsLayoutSchema);
 
 // Definisci le rotte
-app.get('/api/boulders', async (req, res) => {
+app.post("/api/boulders", async (req, res) => {
+  logger(req.body);
+  const { grades } = req.body;
+  let boulders;
+  if (grades.length > 0) {
+    boulders = await Boulder.find({ grade: { $in: grades } });
+  } else {
+    boulders = await Boulder.find();
+  }
   try {
-    const boulders = await Boulder.find();
     res.json(boulders);
+    console.log("Lista boulder recuperata correttamente");
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 });
 
-app.get('/api/getLayout', async (req, res) => {
+app.get("/api/getLayout", async (req, res) => {
   try {
-    const layout = await HoldsLayout.find({"name": "Layout 2024"});
+    const layout = await HoldsLayout.find({ name: "Layout 2024" });
     res.json(layout);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 });
 
-app.post('/api/setLayout', async (req, res) => {
+app.post("/api/setLayout", async (req, res) => {
   const holdsLayout = new HoldsLayout();
   holdsLayout.name = "Layout 2024";
   holdsLayout.holds = process.env.holdsLayout;
@@ -60,34 +66,44 @@ app.post('/api/setLayout', async (req, res) => {
   }
 });
 
-app.post('/api/boulder', async (req, res) => {
-  console.log(req.body)
-  const boulder = new Boulder(req.body);
+app.post("/api/boulder", async (req, res) => {
   try {
+    const boulder = new Boulder(req.body);
+    const existingBoulder = await Boulder.findOne({ name: boulder.name });
+    if (existingBoulder) {
+      console.log("Nome boulder giá presente");
+      return res.status(409).json("Nome boulder giá utilizzato");
+    }
     await boulder.save();
-    console.log("dopo risposta: " + boulder);
+    console.log("Boulder " + boulder.name + " salvato corretamente");
     res.status(201).json(boulder);
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
 });
 
+app.delete("/api/deleteBoulder", async (req, res) => {
+  try {
+    const boulderId = req.body;
+    const boulder = await Boulder.findOneAndDelete({ _id: boulderId });
 
-app.delete('/api/deleteBoulder', async (req, res) => {
-    try{
-        const boulderId = req.body;
-        const boulder = await Boulder.findOneAndDelete({_id: boulderId});
-        
-        if(!boulder){
-            return res.status(404).json({message: 'Boulder non trovata'});
-        }
-        res.status(200).json({ message: 'Boulder eliminato con successo' });
-    } catch(error){
-        console.log(error);
-        res.status(500).json({ message: 'Errore durante l\'eliminazione del boulder' });
+    if (!boulder) {
+      return res.status(404).json({ message: "Boulder non trovata" });
     }
-  });
+    res.status(200).json({ message: "Boulder eliminato con successo" });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ message: "Errore durante l'eliminazione del boulder" });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
+
+function logger(data) {
+  const now = new Date();
+  console.log(`[${now.toISOString()}]`, data);
+}
