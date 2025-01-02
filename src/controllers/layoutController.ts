40a018a85668f32316947ef9a66d89b596d36f1c
GianbaBotassi <gianba89@gmail.com>
2025-01-02 10:17:04 +0100

    Test 3

--- src/controllers/layoutController.ts
+++ src/controllers/layoutController.ts
@@ -1,7 +1,35 @@
 import { Request, Response } from "express";
 import { findLayout } from "../services/layoutService";
+import HoldsLayout from "../models/HoldsLayout";
 
 export const getLayout = async (req: Request, res: Response): Promise<void> => {
   const boulders = await findLayout();
   res.json(boulders);
+};
+
+export const testWithoutDb = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const layout = await HoldsLayout.findOne({
+      _id: "6751b7698b0501e1e2d85ecd",
+    });
+    res.status(200).send("OK");
+  } catch (error) {
+    res.status(404).send("not work");
+  }
+};
+export const testWithDb = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const layout = await HoldsLayout.findOne({
+      _id: "6751b7698b0501e1e2d85ecd",
+    });
+    res.status(200).json(layout);
+  } catch (error) {
+    res.status(404).send("PRoblemmmm");
+  }
 };
--- src/routes/layoutRoutes.ts
+++ src/routes/layoutRoutes.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import { getLayout } from "../controllers/layoutController";
+import { testWithoutDb } from "../controllers/layoutController";
+import { testWithDb } from "../controllers/layoutController";
 
 const router: Router = Router();
 
 router.get("/", getLayout);
+router.get("/testWithoutDb", testWithoutDb);
+router.get("/testWithDb", testWithDb);
 
 export default router;
